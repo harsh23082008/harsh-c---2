1787399d1585f7135a644d63dc3fad94897185ac
harsh23082008 <harshsdv54@gmail.com>
2025-07-19 13:42:47 +0530

    Update 15th.cpp

--- 15th.cpp
+++ 15th.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main() {
     string str, rev;
-    cout << "🔄 Enter a word: ";
+    cout << " Enter a word: ";
     cin >> str;
 
     rev = string(str.rbegin(), str.rend());
     if (str == rev)
-        cout << "✅ It's a palindrome!\n";
+        cout << " It's a palindrome!\n";
     else
-        cout << "❌ Not a palindrome.\n";
+        cout << " Not a palindrome.\n";
 
     return 0;
 }
